ebf087b233301b879358e0c0f06de26e873d327d
Balthier <rrodrijose@gmail.com>
2022-06-18 14:32:03 -0500

    fixed the callback issue

--- index.js
+++ index.js
@@ -3,28 +3,31 @@ move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
 move(character).to(100, 250)
-*/const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
-function moveCharacter(){
-    if(direction==='west'{
-    x=x-1
-})  
-    if(direction==='north'{
-    y=y+1
-})
-    if(direction==='east'{
-    x=x+1
-})
-    if(direction==='south'{
-    x=x-1
-})
-character.style.left=x+'px'
-character.style.bottom=y+'px'
-}
+*/
 
-setInterval(moveCharacter, 1)                                    
+const character = newImage('assets/green-character/static.gif')
+function charDirection(direction){
+    if(direction === null){
+    character.src = 'assets/green-character/static.gif'
+}
+    if(direction === 'west'){
+    character.src = 'assets/green-character/west.gif'
+}
+    if(direction === 'north'){
+    character.src = 'assets/green-character/north.gif'
+}
+    if(direction === 'east'){
+    character.src = 'assets/green-character/east.gif'
+}
+    if(direction === 'south'){
+    character.src = 'assets/green-character/south.gif'
+}
+}
+move(character).withArrowKeys(100, 250, charDirection)
+
+
+
+                                  
 
 
 move(newImage('assets/tree.png')).to(200, 450)
